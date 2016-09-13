1ee5c956205c5242098d275707cbb00a64d9ac2d
GtrCraft <nickvanbokhorst@hotmail.com>
2016-09-13 10:45:20 +0200

    Add Hammerhead

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -21,6 +21,10 @@ ifneq ($(filter bullhead,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/custom/prebuilt/bootanimation/bullhead.zip:system/media/bootanimation.zip
 endif
+ifneq ($(filter pure_hammerhead,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/custom/prebuilt/bootanimation/hammerhead.zip:system/media/bootanimation.zip
+endif
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/custom/prebuilt/bootanimation/shamu.zip:system/media/bootanimation.zip
